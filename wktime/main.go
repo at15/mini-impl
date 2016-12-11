28df3f512ac4146c18c2fe1729e8cf573e87dfd5
at15 <at15@outlook.com>
2016-12-10 16:53:59 -0800

    [wktime] can't get data when proxy https
    so need to fork the plugin and the collector cli

--- wktime/main.go
+++ wktime/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/elazarl/goproxy"
@@ -11,12 +12,26 @@ import (
 func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
-	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		// log request body
+	// proxy.OnRequest(goproxy.DstHostIs("www.baidu.com")).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	// 	// log request body
+	// 	log.Println("I am doing the func!")
+	// 	body, _ := ioutil.ReadAll(req.Body)
+	// 	log.Println(body)
+	// 	return req, nil
+	// })
+
+	// this works for http, but does not work for https
+	// proxy.OnRequest(goproxy.DstHostIs("www.baidu.com")).DoFunc(
+	// 	func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	// 		return r, goproxy.NewResponse(r,
+	// 			goproxy.ContentTypeText, http.StatusForbidden,
+	// 			"Don't waste your time!")
+	// 	})
+
+	proxy.OnRequest(goproxy.DstHostIs("www.baidu.com")).HijackConnect(func(req *http.Request, client net.Conn, ctx *goproxy.ProxyCtx) {
 		log.Println("I am doing the func!")
 		body, _ := ioutil.ReadAll(req.Body)
 		log.Println(body)
-		return req, nil
 	})
 	log.Println("start proxy")
 	log.Fatal(http.ListenAndServe(":9999", proxy))
